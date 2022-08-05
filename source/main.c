8fac034fed8e94cb63756dcb9dc00e9eda33e88e
LaoZhu <2208213223@qq.com>
2022-08-05 23:00:42 +0800

    Rewrote the breathing light effect to make the effect look better.

--- source/main.c
+++ source/main.c
@@ -2,34 +2,34 @@
 
 TaskHandle_t led_control_taskhandler;
 
+void show_color_with_lightning(unsigned char lightning) {
+    for (unsigned char i = 0; i < 6; ++i) {
+        select_small_board(i);
+        ws2812_set_all_color(lightning / 4, lightning, lightning);
+        ws2812_show_color();
+        delayms(10);
+    }
+}
+
 void led_control(void *pvParameters) {
     (void) pvParameters;
-    unsigned char lightning = 0;
+    unsigned char lightning = 0, direction = 0;
     delayms(200);
     while (1) {
-        lightning = (lightning + 1) % 110;
-        for (unsigned char i = 0; i < 6; ++i) {
-            select_small_board(i);
-            ws2812_set_all_color(lightning / 4, lightning, lightning);
-            ws2812_show_color();
-            delayms(10);
-        }
-        delayms(4);
-
-        if (lightning > 80) {
-            while (1) {
-                lightning = lightning - 1;
-                for (unsigned char i = 0; i < 6; ++i) {
-                    select_small_board(i);
-                    ws2812_set_all_color(lightning / 4, lightning, lightning);
-                    ws2812_show_color();
-                    delayms(5);
-                }
-                delayms(4);
-                if (lightning == 0)
-                    break;
+        if (direction == 0) {
+            lightning = lightning + 1;
+            show_color_with_lightning(lightning);
+            delayms(20);
+            if (lightning == 25)
+                direction = 1;
+        } else {
+            lightning = lightning - 1;
+            show_color_with_lightning(lightning);
+            delayms(20);
+            if (lightning == 0) {
+                delayms(300);
+                direction = 0;
             }
-            delayms(100);
         }
     }
 }
